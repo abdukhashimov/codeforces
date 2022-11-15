66f733cb43c66be05aee3bebcf1bb4a45fce3f33
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2022-11-15 15:58:33 +0500

    tried the template for cpp

--- sample.cpp
+++ sample.cpp
@@ -1,12 +1,50 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define endl '\n'
+#define tc     \
+    ll tc;     \
+    cin >> tc; \
+    while (tc--)
+#define pb push_back
+#define mp make_pair
+const ll MOD = 1e9 + 7;
 
-int main(int argc, char const *argv[])
+void fastio()
 {
-    int temp;
-    cin >> temp;
-    cout << "here";
-    cout << temp * 10;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int x;
+    cin >> x;
+    cout << binarySearch(a, 0, n - 1, x);
+
+    return 0;
+}
+int main()
+{
+    fastio();
+    ll t;
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
     return 0;
 }

83e5e2ee4dfb056c02184a396fd4d6c7c2d12439
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2022-11-18 20:26:31 +0500

    add termostat

---
+++ C_Thermostat.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define tc     \
+    ll tc;     \
+    cin >> tc; \
+    while (tc--)
+#define pb push_back
+#define mp make_pair
+const ll MOD = 1e9 + 7;
+
+void fastio()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    fastio();
+    ll t;
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}

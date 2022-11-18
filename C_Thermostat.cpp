671f7056b07b1e57d27850b32824b4795bafb151
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2022-11-18 21:33:42 +0500

    add no solution

--- C_Thermostat.cpp
+++ C_Thermostat.cpp
@@ -17,8 +17,48 @@ void fastio()
     cout.tie(0);
 }
 
+int findMin(map<int, bool> numbers, int x, int dest, int max_num, int min_num)
+{
+    if (numbers.find(dest) != numbers.end())
+    {
+        return 0;
+    }
+
+    for (auto &[key, value] : numbers)
+    {
+        for (int i = x; i <= max_num; i++)
+        {
+            if (key + i <= max_num)
+            {
+                numbers[key + i] = true;
+            }
+        }
+
+        for (int i = min_num; i <= x; i++)
+        {
+            if (i >= 0 && key - i <= max_num && key - i >= min_num)
+            {
+                numbers[key - i] = true;
+            }
+        }
+    }
+
+    for (auto &&key : numbers)
+    {
+        cout << key.first << endl;
+    }
+
+    return 1 + findMin(numbers, x, dest, max_num, min_num);
+}
+
 void solve()
 {
+    int l, r, x, a, b;
+    cin >> l >> r >> x >> a >> b;
+    map<int, bool> numbers = {
+        {a, true}};
+
+    cout << findMin(numbers, x, b, r, l) << endl;
 }
 
 int main()

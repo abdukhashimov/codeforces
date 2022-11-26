d7ed1c70f5086856f02b338cb7537ed3b674577d
Madiyor Abdukhashimov <abdukhashimov@yandex.ru>
2022-11-26 10:07:10 +0500

    add almost multiple

---
+++ C_Almost_All_Multiples.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define tc     \
+    ll tc;     \
+    cin >> tc; \
+    while (tc--)
+#define pb push_back
+#define mp make_pair
+const ll MOD = 1e9 + 7;
+
+void fastio()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    deque<int> dq;
+
+        dq.push_back(1);
+
+    for (int i = 0; i < dq.size(); i++)
+    {
+        cout << dq[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fastio();
+    ll t;
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
